4a44a9c3058cae47a358954415afe980e9d9125d
DwayneM20 <Dwayne.Mack@lc.cuny.edu>
2026-06-12 00:32:56 -0400

    first function: scan and print number of wifi networks

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Board starting up...");
+  delay(1000);
+  Serial.println("Starting WiFi scan...");
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(1000);
+  int numNetworks = WiFi.scanNetworks();
+  if (numNetworks < 0)
+  {
+    Serial.println("WiFi scan failed");
+    return;
+  }
+  Serial.println("Scan complete.");
+  Serial.print("Number of networks found: ");
+  Serial.println(numNetworks);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
